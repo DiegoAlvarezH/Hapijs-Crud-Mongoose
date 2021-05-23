76fc0b0c2bfec0b880efac5d316cd045448af76b
Diego Alvarez <diegoah905@gmail.com>
2021-05-22 19:05:12 -0500

    the file is add database, connection for MongoDB, with mongoose

--- src/database.js
+++ src/database.js
@@ -1,1 +1,11 @@
 //Esta sera la conexion a la BD
+const mongoose = require('mongoose');
+
+require('./database')
+
+mongoose.connect('mongodb://localhost/testhapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Database is connected'))
+.catch(err => console.log(err));
